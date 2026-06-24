96dc4efb0e5a3160e0d7ff87e3e3eaded36273a3
Anamul Haque <anamulhaque4631@gmail.com>
2026-06-24 18:13:31 +0600

    Calculate delivery charge and others

--- src/Pages/SendParcels/SendParcel.jsx
+++ src/Pages/SendParcels/SendParcel.jsx
@@ -1,5 +1,6 @@
 import { useForm, useWatch } from "react-hook-form";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const SendParcel = () => {
   const {
@@ -30,8 +31,48 @@ const SendParcel = () => {
   };
 
   const handleSendParcel = (data) => {
-    const sameDistrict = data.senderDistrict === data.receiverDistrict;
-    console.log(sameDistrict);
+    const isDocument = data.parcelType === "document";
+
+    const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+
+    const parcelWeight = parseFloat(data.parcelWeight);
+
+    let cost = 0;
+
+    if (isDocument) {
+      cost = isSameDistrict ? 60 : 80;
+    } else {
+      if (parcelWeight < 3) {
+        cost = isSameDistrict ? 110 : 150;
+      } else {
+        const minCharge = isSameDistrict ? 110 : 150;
+
+        const extraWeight = parcelWeight - 3;
+
+        const extraCharge = isSameDistrict
+          ? extraWeight * 40
+          : extraWeight * 40 + 40;
+        cost = minCharge + extraCharge;
+      }
+    }
+
+    Swal.fire({
+      title: "Agree with the cost?",
+      text: `You  will be charged! ${cost} TK`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: " I agree to pay!",
+    }).then((result) => {
+      if (result.isConfirmed)
+        
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+    });
   };
 
   return (
