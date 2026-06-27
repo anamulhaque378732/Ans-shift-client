df017a5513827a85e823715312f32d223f498d21
Anamul Haque <anamulhaque4631@gmail.com>
2026-06-27 12:38:36 +0600

    Showt parcel filter bye email, and delete parcel data

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -36,7 +36,6 @@ const DashboardLayout = () => {
           </nav>
           {/* Page content here */}
           <Outlet></Outlet>
-          <div className="p-4">Page Content</div>
         </div>
 
         <div className="drawer-side is-drawer-close:overflow-visible">
--- src/Pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/UseAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteForever, MdOutlineSecurityUpdate } from "react-icons/md";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["myParcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
@@ -14,12 +18,92 @@ const MyParcels = () => {
       return res.data;
     },
   });
+  const handleParcelDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          if (res.data.deletedCount) {
+            // refresh the data in the ui
+
+            refetch();
+
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel request has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
       <h2 className="text-3xl md:my-12 py-2 font">
         All of my parcel : {parcels.length}
       </h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th> Payment status</th>
+              <th> Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td> {parcel.parcelName}</td>
+                <td> {parcel.parcelWeight}</td>
+                <td>true</td>
+                <td className="">
+                  <button
+                    title="Edit"
+                    className="btn btn-square mr-1 hover:bg-secondary hover:text-white"
+                  >
+                    <CiEdit />
+                  </button>
+                  <button
+                    title="Update"
+                    className="btn   btn-square mr-1 hover:bg-secondary hover:text-white"
+                  >
+                    <MdOutlineSecurityUpdate />
+                  </button>
+
+                  <button
+                    title="View details"
+                    className="btn btn-square hover:bg-secondary mr-1 hover:text-white"
+                  >
+                    <FaMagnifyingGlass />
+                  </button>
+                  <button
+                    onClick={() => handleParcelDelete(parcel._id)}
+                    title="Delete"
+                    className="btn btn-square hover:bg-secondary hover:text-white"
+                  >
+                    <MdDeleteForever />
+                  </button>
+                </td>
+              </tr>
+            ))}
+            {/* row 1 */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
