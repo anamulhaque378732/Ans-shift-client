1574eda6dd5668c5d73be7dd349dcf0414b52269
Anamul Haque <anamulhaque4631@gmail.com>
2026-06-30 21:31:39 +0600

    Transaction id and tracking id add and sshow a table

--- src/Pages/Dashboard/MyParcels/Payment/PaymentSuccess.jsx
+++ src/Pages/Dashboard/MyParcels/Payment/PaymentSuccess.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const axiosSecure = useAxiosSecure();
   const sessionId = searchParams.get("session_id");
 
@@ -11,13 +12,24 @@ const PaymentSuccess = () => {
     if (sessionId) {
       axiosSecure
         .patch(`payment-success?session_id=${sessionId}`)
-        .then((res) => console.log(res.data));
+        .then((res) => {
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
+        });
     }
   }, [sessionId, axiosSecure]);
 
   return (
     <div>
-      <h2 className="text-4xl font-bold my-4 py-4">Payment successful</h2>
+      <h2 className="text-4xl font-bold my-4 py-4">Payment successful :</h2>
+      <p className="text-xl my-2 py-2 font-medium">
+        Your transactionId: {paymentInfo.transactionId}
+      </p>
+      <p className="text-xl my-2 py-2 font-medium">
+        Your trackingId : {paymentInfo.trackingId}
+      </p>
     </div>
   );
 };
